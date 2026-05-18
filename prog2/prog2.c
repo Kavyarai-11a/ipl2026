056404062c51c2d1c4ec40427cef2fa778a1dc91
Kavyarai-11a <kavyarai812@gmail.com>
2026-05-18 15:28:53 +0530

    the prog 2

---
+++ prog2/prog2.c
@@ -0,0 +1,76 @@
+#include<stdio.h>
+typedef struct {
+    int id;
+    char name[20];
+    float salary;
+} Employee;
+void seek_position(int n,int arr[],Employee employees[],const char*filename);
+void read_details(int m,int arr[],Employee employees[],const char* filename);
+int main(){
+    int n;
+    printf("Enter the number of employees: ");
+    scanf("%d", &n);
+    if(n <= 0)
+    {
+        printf("Invalid number of employees.\n");
+        return 0;
+    }
+    Employee employees[n];
+    int arr[n+1];
+    for(int i=0; i<n; i++){
+        printf("Enter details of employee %d:\n", i);
+        printf("ID: ");
+        scanf("%d", &employees[i].id);
+        printf("Name: ");
+        scanf("%s", employees[i].name);
+        printf("Salary: ");
+        scanf("%f", &employees[i].salary);
+    }
+    seek_position(n, arr,employees, "employees.txt");
+    int m;
+    printf("Enter the index of the employee whose details you want to display: ");
+    scanf("%d", &m);
+    if(m<0||m>=n)
+    {
+        printf("Invalid index.\n");
+        return 0;
+    }
+    read_details(m,arr,employees, "employees.txt");
+    return 0;
+}
+void seek_position(int n,int arr[],Employee employees[],const char*filename){
+    FILE *fp=fopen("employees.txt","w");
+    if(fp == NULL)
+    {
+        printf("Error opening file.\n");
+        return;
+    }
+    arr[0]=0;
+    int i=0;
+    int count=0;
+    while(i<n)
+    {
+        
+        
+        int value=fprintf(fp, "%d %s %.2f\n", employees[i].id, employees[i].name, employees[i].salary);
+        count=count+value;
+        arr[i+1]=count;
+        i++;
+    }
+    fclose(fp);
+}
+void read_details(int m,int arr[],Employee employees[],const char* filename){
+    FILE *fp=fopen(filename,"r");
+    if(fp == NULL)
+    {
+        printf("Error opening file.\n");
+        return;
+    }
+    fseek(fp, arr[m], SEEK_SET);
+    fscanf(fp, "%d %s %f", &employees[m].id, employees[m].name, &employees[m].salary);
+    fclose(fp);
+    printf("Employee details at index %d:\n", m);
+    printf("ID: %d\n", employees[m].id);
+    printf("Name: %s\n", employees[m].name);
+    printf("Salary: %.2f\n", employees[m].salary);
+}
